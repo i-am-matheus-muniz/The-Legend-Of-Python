70609544c6952fdfe699ad65c9324afd6952daf1
Matheus Muniz <i.am.matheus.muniz@gmail.com>
2024-04-13 15:58:54 -0300

    Python learning progress

---
+++ 08-BMI.py
@@ -0,0 +1,6 @@
+mass = 80
+height = 174
+
+bmi = mass/(height ** 2)
+
+print(bmi)
---
+++ 09-pythagorean.py
@@ -0,0 +1,6 @@
+a = int(input('Enter the length short side of the triangle: '))
+b = int(input('Enter the length of another short side of the triangle: '))
+
+c = ((a ** 2) + (b ** 2)) ** 0.5
+
+print('Pythagore: ', c)
