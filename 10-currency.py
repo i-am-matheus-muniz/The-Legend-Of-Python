52ddc8eacc95d4c2ec6dbb9faa23ae964bc4764f
Matheus Muniz <i.am.matheus.muniz@gmail.com>
2024-04-13 22:49:20 -0300

    More python studies

---
+++ 10-currency.py
@@ -0,0 +1,5 @@
+co = float(input('What do you have left in pesos? ')) / 863.67
+pe = float(input('What do you have left in soles? ')) / 3.68
+br = float(input('What do you have left in reais? ')) / 5.08
+
+print('You have ', co+pe+br, ' US dollars!')
